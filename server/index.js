13d1eaf7133c3fb32eeccda714b0f992a6045b59
Sarfaraj028 <sarfaraja0869@gmail.com>
2025-06-11 10:58:48 +0530

    adding errorMiddleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-import express from "express"
+import express, { json } from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import apiRoute from "./routes/apiRoutes.js"
 import { connectDB } from "./db/db.js"
+import { errorMiddleware } from "./middlewares/errorMiddleware.js"
 
 const app = express()
 dotenv.config()
@@ -10,22 +11,30 @@ connectDB()
 
 const PORT = process.env.SERVER_PORT || 3000
 
+//middlewares 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 //allow this frontend to access the backend
 app.use(cors({
   origin: process.env.FRONTEND_URL, // frontend port
   credentials: true
 }));
 
+// routes 
 app.get("/", (req, res)=>{
     res.send("Home page")
 })
 
-//api 
+//api routes
 app.use("/api", apiRoute)
 
+// page not found
 app.use((req, res) =>{
-    res.send("Page Not Found 😢")
+    res.status(404).json({message: "Page Not Found 😢"})
 })
+//global Error handler
+app.use(errorMiddleware)
 
 app.listen(PORT, ()=>{
     console.log("App is running on the",PORT);
